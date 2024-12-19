a5739945549dcc0411489a56086704516b839ebc
luizsolely <solelydev1@gmail.com>
2024-12-19 17:40:50 -0300

    first commit

--- front-end-react1/web1/src/App.js
+++ front-end-react1/web1/src/App.js
@@ -94,22 +94,6 @@ function App() {
           </table>
         </div>
       )}
-
-      <div id='project-info-card'>
-        <h1 id='project-info-title'>REACT + EXPRESS</h1>
-        <h4 id='project-info-subtitle'>APLICAÇÃO FEITA POR:</h4>
-        <ul className='integrants-list'>
-          <li className='integrant'>André Luiz Nascimento de Andrade <span>CB3032256</span></li>
-          <li className='integrant'>Auan Julio Galvão dos Santos <span>CB3030369</span></li>
-          <li className='integrant'>Geovanna Barros de Asssunção <span>CB303271X</span></li>
-          <li className='integrant'>Luiz Felipe Gonçalves da Silva <span>CB3030539</span></li>
-          <li className='integrant'>João do Valle Seixas Paula <span>CB3030784</span></li>
-          <li className='integrant'>Paulo Eduardo da Silva Pessoa <span>CB303092X</span></li>
-      </ul>
-      <p>Aplicação feita com o uso de:<br />React <span>v18.3.1</span> <br />Node <span>v22.11.0</span><br />Express <span>v4.21.1</span></p>
-      <p>Ferramenta de avaliação da disciplina de Desenvolvimento Web Avançado <br />Professor Eduardo Henrique Gomes</p>
-    </div>
-
     </div>
     
   );
